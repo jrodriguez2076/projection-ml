1eedb2887c84793c3759f47b84854d62ccb2ab9a
Jose <jrodriguez2076@gmail.com>
2020-09-30 08:32:13 -0300

    finished file setup

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = ()=>{
+    return <h3>
+        React works!
+    </h3>
+}
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('app')
+    );
